73b65aa57ce990fb7c8a055b23b21d58d93b3545
Marcio Borges <marciojsb@hotmail.com>
2026-09-04 23:28:54 +0000

    Finsh `/api/v1/status`endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,42 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "ok" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =  $1;",
+    values: [databaseName],
+  });
+
+  console.log(
+    "Database Version:",
+    databaseVersionResult.rows[0].server_version,
+  ); // Log the database info for debugging
+  console.log(
+    "Max Connections:",
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+  console.log(
+    "Opened Connections:",
+    databaseOpenedConnectionsResult.rows[0].count,
+  ); // Log the number of opened connections for debugging
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnectionsResult.rows[0].max_connections,
+        ),
+        open_connections: databaseOpenedConnectionsResult.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
